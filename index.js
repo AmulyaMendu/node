b8d198b28c16dac2ff8bf809b8908ebcaf90c571
AmulyaMendu <amulyasamala666@gmail.com>
2023-09-11 18:54:57 +0530

    updated one

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const app=express();
 app.get("/",(req,res)=>{
     res.send("<h1>hello shyam</h1>")
 })
+app.post("/home",(req,res)=>{
+    res.send("<h1>hello janvika</h1>")
+})
 app.listen(3000,()=>console.log("server is up"))
